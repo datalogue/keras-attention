4f872b7e227fa9857f19bd62d6d384defc3c234f
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-05-27 22:58:17 -0400

    Added horizontal legend to maps

--- visualize.py
+++ visualize.py
@@ -3,6 +3,7 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 from models.NMT import simpleNMT
 from utils.examples import run_example
@@ -59,16 +60,35 @@ class Visualizer(object):
         activation_map = np.squeeze(self.proba_model.predict(np.array([d])))[
             0:output_length, 0:input_length]
 
+        # import seaborn as sns
         plt.clf()
-        plt.imshow(activation_map, interpolation='nearest')
-        plt.legend(loc='best')
-        plt.yticks(range(output_length), predicted_text[:output_length])
-        plt.xticks(range(input_length), text_[:input_length], rotation=45)
-        plt.xlabel('Input Sequence')
-        plt.ylabel('Output Sequence')
-        plt.grid()
-        plt.savefig('./attention_maps/'+text.replace('/', '')+'.pdf', bbox_inches='tight')
+        f = plt.figure(figsize=(8, 8.5))
+        ax = f.add_subplot(1, 1, 1)
 
+        # add image
+        i = ax.imshow(activation_map, interpolation='nearest', cmap='gray')
+        
+        # add colorbar
+        cbaxes = f.add_axes([0.2, 0, 0.6, 0.03])
+        cbar = f.colorbar(i, cax=cbaxes, orientation='horizontal')
+        cbar.ax.set_xlabel('Probability', labelpad=2)
+
+        # add labels
+        ax.set_yticks(range(output_length))
+        ax.set_yticklabels(predicted_text[:output_length])
+        
+        ax.set_xticks(range(input_length))
+        ax.set_xticklabels(text_[:input_length], rotation=45)
+        
+        ax.set_xlabel('Input Sequence')
+        ax.set_ylabel('Output Sequence')
+
+        # add grid and legend
+        ax.grid()
+        # ax.legend(loc='best')
+
+        f.savefig('./attention_maps/'+text.replace('/', '')+'.pdf', bbox_inches='tight')
+        f.show()
 
 def main(examples, args):
     print('Total Number of Examples:', len(examples))
